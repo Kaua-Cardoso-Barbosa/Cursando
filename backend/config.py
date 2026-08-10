51aa1dd45ca450c5dfb56ddae52949473fa26e6e
diogolopesnunes <gran34sudaruska@gmail.com>
2026-08-10 14:29:35 -0300

    Main.py e Config.py criados

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+import os
+
+SECRET_KEY = 'Secret Key bem bonita do sistema'
+DEBUG = True
+
+DB_HOST = 'localhost'
+DB_NAME = r''
+DB_USER = 'sysdba'
+DB_PASSWORD = 'sysdba'
+
+UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__))
